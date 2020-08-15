f27af2639a1067c27f2b642e3e40c38f4d009696
Daniel Pena Berroa <digrape07@gmail.com>
2020-08-15 12:34:34 -0400

    stest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [showInstallButton, setShowInstallButton] = useState(false);
+
+  let deferredPrompt;
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+
+      if (showInstallButton === false) {
+        setShowInstallButton(true);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +27,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      {showInstallButton ? <button>Isntall!</button> : undefined}
     </div>
   );
 }
